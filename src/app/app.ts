a535094c3554cb02acffd6b6606263ea247ccbdc
GabrieSVasc <vascgabrie@gmail.com>
2026-07-15 19:05:04 -0300

    feat: adicionando o html do componente de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SideMenu } from "./side-menu/side-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [SideMenu],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
