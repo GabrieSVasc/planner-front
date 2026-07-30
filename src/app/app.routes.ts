227ade820edc175a277ec01abf130d4c2a0ddb9c
GabrieSVasc <vascgabrie@gmail.com>
2026-07-30 10:28:43 -0300

    feat: desenvolvendo telas de meta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { LoginPage } from './login-page/login-page';
 import { RegisterPage } from './register-page/register-page';
 import { ListMetas } from './list-metas/list-metas';
+import { FormMeta } from './form-meta/form-meta';
 
 export const routes: Routes = [
     {
@@ -28,5 +29,13 @@ export const routes: Routes = [
     {
         path: 'metas',
         component: ListMetas,
+    },
+    {
+        path: 'metas/criar',
+        component: FormMeta
+    },
+    {
+        path: 'metas/editar/:id',
+        component: FormMeta
     }
 ];
--- src/app/list-metas/list-metas.ts
+++ src/app/list-metas/list-metas.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideMenu } from "../side-menu/side-menu";
+import { Meta } from '../meta';
+import { MetaService } from '../meta-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-metas',
@@ -7,4 +10,30 @@ import { SideMenu } from "../side-menu/side-menu";
   templateUrl: './list-metas.html',
   styleUrl: './list-metas.css',
 })
-export class ListMetas {}
+export class ListMetas implements OnInit {
+  constructor(
+    private metaService: MetaService,
+    private router: Router
+  ){}
+  metas: Meta[] = [];
+
+  ngOnInit(): void{
+    this.renovarLista();
+  }
+  
+  renovarLista(){
+    this.metas = this.metaService.getMetas();
+  }
+  editMeta(id?: number){
+    this.router.navigate(['/metas/editar/'+id]);
+  }
+
+  removeMeta(id?: number){
+    this.metaService.removeMeta(id);
+    this.renovarLista();
+  }
+
+  to(rota: string){
+    this.router.navigate(['/metas/'+rota]);
+  }
+}
