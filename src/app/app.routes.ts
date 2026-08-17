eb424d126379415fe1751ae6a3a69da2fb9abcdb
w-torresdev <williantorreskk@gmail.com>
2026-08-17 00:05:15 -0300

    feat: define rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastroPage } from './cadastro-page/cadastro-page';
 import { LoginPage } from './login-page/login-page';
+import { TasksPage } from './tasks-page/tasks-page';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LoginPage
-    },
+  {
+    path: '',
+    component: LoginPage,
+    pathMatch: 'full',
+  },
+  {
+    path: 'cadastro',
+    component: CadastroPage,
+  },
+  {
+    path: 'tarefas',
+    component: TasksPage,
+  },
 ];
